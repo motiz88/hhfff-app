8b1e2c64c8024c756b5b18658c88a17e3179173a
Moti Zilberman <moti@digital-detox.co.uk>
2017-04-10 18:25:13 +0300

    Set up 30sec notification for testing

--- utils/setupNotifications.js
+++ utils/setupNotifications.js
@@ -4,12 +4,14 @@ import Expo from "expo";
 import { version } from "../package.json";
 import moment from "moment";
 
+const DEV = true;
+
 async function setupNotifications() {
   const savedVersion = await AsyncStorage.getItem("notifications.savedVersion");
-  if (savedVersion !== version) {
+  if (savedVersion !== version || DEV) {
     await Expo.Notifications.cancelAllScheduledNotificationsAsync();
     await Promise.all(
-      data.FilmsIndex.byStartTime.map(async filmId => {
+      data.FilmsIndex.byStartTime.map(async (filmId, i) => {
         const film = data.Films[filmId];
         const {
           title,
@@ -38,22 +40,28 @@ async function setupNotifications() {
         if (notificationTime.isAfter(exactStartTime)) {
           notificationTime.subtract(1, "day");
         }
-        return await Expo.Notifications.scheduleLocalNotificationAsync(
-          {
-            title: title + (venue.is_outdoor ? " (Outdoor screening)" : ""),
-            body: `${time.start} at ${venue.name}`,
-            data: JSON.stringify({
-              film: {
-                filmId
-              }
-            }),
-            android: {
-              color: colors.highlight,
-              vibrate: true
+        const notification = {
+          title: title + (venue.is_outdoor ? " (Outdoor screening)" : ""),
+          body: `${time.start} at ${venue.name}`,
+          data: JSON.stringify({
+            film: {
+              filmId
             }
-          },
-          { time: notificationTime.toDate() }
-        );
+          }),
+          android: {
+            color: colors.highlight,
+            vibrate: true
+          }
+        };
+        await Expo.Notifications.scheduleLocalNotificationAsync(notification, {
+          time: notificationTime.toDate()
+        });
+        if (DEV && i === 5) {
+          await Expo.Notifications.scheduleLocalNotificationAsync(
+            notification,
+            { time: moment().add(30, "seconds").toDate() }
+          );
+        }
       })
     );
     await AsyncStorage.setItem("notifications.savedVersion", version);
